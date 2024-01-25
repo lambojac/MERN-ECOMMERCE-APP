4bfd628224b1df456404c5aa4dfe5e60a83840e2
lambojac <you@example.com>
2024-01-25 12:54:17 +0100

    es6 format wsa used in package.json

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express=require("express")
+import express from "express"
+import  bgCyan from "colors"
 
-const app= express()
-
+const app=express()
 app.get("/",(req,res)=>{
     res.send({
         message:"welcome to ecommer app"
     })
 })
-
+//PORT
 const PORT =8080
+app.listen(PORT,()=>{
+console.log(`server running on ${PORT}`.bgCyan.white);
+})
