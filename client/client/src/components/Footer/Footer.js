9d46233a7c7c64e60d1cdceeb39ed08f4836598f
adamokennie <adamokennie@gmail.com>
2024-03-03 23:50:21 +0100

    styling

--- client/client/src/components/Footer/Footer.js
+++ client/client/src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Footer.css"
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
-      <h1 className='text-center'>All Right Reserved &copy;2024</h1>
+    <div className="footer">
+      <h1 className='text-center'>All Right Reserved &copy;Adamo Kehinde</h1>
     </div>
   )
 }
