af15c6e5627bb5d6a58e0499d3dacffaa9a4c062
adamokennie <adamokennie@gmail.com>
2024-03-10 16:56:47 +0100

    about us page

--- client/client/src/components/Layout/Layout.js
+++ client/client/src/components/Layout/Layout.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Header from "../Header/Header.js"
 import Footer from "../Footer/Footer"
-const Layout = ({children}) => {
+import {Helmet} from "react-helmet"
+const Layout = ({children, content, description, keywords, author}) => {
   return (
     <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description}/>
+                <meta name="keywords" content={keywords}/>
+                <meta name="author" content={author}/>
+                <title>My Title</title>
+      </Helmet>
         <Header/>
         <main style={{minHeight:"80vh"}}>{children}</main>
        <Footer/>
--- client/client/src/pages/About.js
+++ client/client/src/pages/About.js
@@ -1,14 +1,25 @@
- import React from 'react'
- import Layout from "../components/Layout/Layout"
- const About = () => {
-   return (
-     <Layout>
-    about us page
-     </Layout>
-       
-     
-   )
- }
- 
- export default About
- 
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"aboutus-ecommerce-app"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            We are Ecommerce company offering buying and selling services to individuals consumers arounf the globe
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
